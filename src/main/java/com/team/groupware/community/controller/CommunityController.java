2f0c40da0460abe5813f5814fe1ca7be05286ae1
User <User@UserPC>
2017-04-11 16:53:03 +0900

    김효원 - 패키지 컨트롤러 작성

--- src/main/java/com/team/groupware/community/controller/CommunityController.java
+++ src/main/java/com/team/groupware/community/controller/CommunityController.java
@@ -1,5 +1,27 @@
 package com.team.groupware.community.controller;
 
-public class CommunityController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.team.groupware.community.model.CommunityVO;
+
+@Controller
+@RequestMapping("/community")
+public class CommunityController {
+	private static final Logger logger=LoggerFactory.getLogger(CommunityController.class);
+	
+	@RequestMapping(value="/write.do", method=RequestMethod.GET)
+	public String write_get(){
+		logger.info("글 작성 보여주기");
+		
+		return "community/write";
+	}
+	
+	@RequestMapping(value="/write.do", method=RequestMethod.POST)
+	public String write_post(CommunityVO vo){
+		logger.info("글 작성 하기 파라미터 vo={}", vo);
+	}
 }
